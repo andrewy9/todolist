5cec917f3bee38717e3fed9312da33ea49939d94
andrewy9 <andrew.yan993@gmail.com>
2020-12-31 16:54:32 +1300

    add and display todo list

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 //Import Components
 import Todo from './Todo'
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
   return (
     <div className="todo-container">
-      <ul className="todo-list"></ul>
-      <Todo />
+      <ul className="todo-list">
+        {todos.map(todo => (
+          <Todo text={todo.text} key={todo.id} />
+        ))}
+      </ul>
     </div>
   )
 }
